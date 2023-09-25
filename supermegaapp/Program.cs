76a1fb1eb8da1f2a2dba2a6a61e6cde32f7c36e9
RxNaison <>
2023-09-25 15:01:43 +0300

    first commit

---
+++ supermegaapp/Program.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace supermegaapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[6, 6];
+
+            Rand();
+
+            A();
+            B();
+            C();
+            D();
+            E();
+            F();
+            G();
+            H();
+            I();
+            J();
+
+            void A()
+            {
+                int max = int.MinValue;
+
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+
+                        if (max < array[i, j] && j >= i)
+                            max = array[i, j];
+
+                    }
+                }
+
+                Console.WriteLine("а: " + max);
+            }
+
+            void B()
+            {
+                int max = int.MinValue;
+
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+
+                        if (max < array[i, j] && j <= i)
+                            max = array[i, j];
+                    }
+                }
+                Console.WriteLine("b: " + max);
+            }
+
+            void C()
+            {
+                int max = int.MinValue;
+
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+
+                        if (max < array[i, j] && j >= i && j <= array.GetLength(0) - i - 1)
+                            max = array[i, j];
+                    }
+                }
+                Console.WriteLine("c: " + max);
+            }
+
+            void D()
+            {
+                int max = int.MinValue;
+
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+
+                        if (max < array[i, j] && j <= i && j >= array.GetLength(0) - i - 1)
+                            max = array[i, j];
+                    }
+                }
+                Console.WriteLine("d: " + max);
+            }
+
+            void E()
+            {
+                int max = int.MinValue;
+
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+
+                        if (max < array[i, j] && j <= i && j >= array.GetLength(0) - i - 1 ||
+                            j >= i && j <= array.GetLength(0) - i - 1 && max < array[i, j])
+                            max = array[i, j];
+                    }
+                }
+                Console.WriteLine("e: " + max);
+            }
+
+            void F()
+            {
+                int max = int.MinValue;
+
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+
+                        if (max < array[i, j] && j >= i && j >= array.GetLength(0) - i - 1 ||
+                            j <= i && j <= array.GetLength(0) - i - 1 && max < array[i, j])
+                            max = array[i, j];
+                    }
+                }
+                Console.WriteLine("f: " + max);
+            }
+
+            void G()
+            {
+                int max = int.MinValue;
+
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+
+                        if (max < array[i, j] && j <= i && j <= array.GetLength(0) - i - 1)
+                            max = array[i, j];
+                    }
+                }
+                Console.WriteLine("g: " + max);
+            }
+
+            void H()
+            {
+                int max = int.MinValue;
+
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+
+                        if (max < array[i, j] && j >= i && j >= array.GetLength(0) - i - 1)
+                            max = array[i, j];
+                    }
+                }
+                Console.WriteLine("h: " + max);
+            }
+
+            void I()
+            {
+                int max = int.MinValue;
+
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+
+                        if (max < array[i, j] && j <= array.GetLength(0) - i - 1)
+                            max = array[i, j];
+                    }
+                }
+                Console.WriteLine("i: " + max);
+            }
+
+            void J()
+            {
+                int max = int.MinValue;
+
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+
+                        if (max < array[i, j] && j >= array.GetLength(0) - i - 1)
+                            max = array[i, j];
+                    }
+                }
+                Console.WriteLine("j: " + max);
+            }
+
+
+
+            void Rand()
+            {
+                Random rand = new Random();
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        array[i, j] = rand.Next(0, 99);
+                    }
+                }
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        Console.Write(array[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ supermegaapp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("supermegaapp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("supermegaapp")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("6f220d92-7daa-43d4-a84b-b0317006b019")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
